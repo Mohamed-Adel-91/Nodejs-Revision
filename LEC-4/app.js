3c0e3f1d2f58c906dc404ff67dea721ef3cf38d1
Mohamed Adel <mohamed101291@gmail.com>
2023-12-24 16:44:26 +0200

    Express

--- LEC-4/app.js
+++ LEC-4/app.js
@@ -4,4 +4,40 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.listen(5001);
+let cars = [
+    { model: "Tesla", year: 2023, color: "Red" },
+    { model: "BMW", year: 2024, color: "Blue" },
+];
+app.get("/cars", function (req, res) {
+    res.json({ car: cars, status: 200 });
+});
+
+app.post("/cars", function (req, res) {
+    console.log(req.body);
+    cars.push(req.body);
+    res.json({ status: 200, message: "Cars add successfully" });
+});
+
+app.put("/cars/:name", function (req, res) {
+    let name = req.params.name;
+    cars.find((o, i) => {
+        if (o.model === name) {
+            cars[i] = req.body;
+            return true;
+        }
+    });
+    res.json({ message: "car is updated", status: 200 });
+});
+
+app.delete("/cars/:name", function (req, res) {
+    let name = req.params.name;
+    cars.find((o, i) => {
+        if (o.model === name) {
+            cars.splice(cars[i], 1);
+            return true;
+        }
+    });
+    res.json({ message: "car is deleted", status: 200 });
+});
+
+app.listen(5050);
