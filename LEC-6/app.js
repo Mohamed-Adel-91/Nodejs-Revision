f2c73da4d881f90e7e10324e31f3462354e5bb91
Mohamed Adel <mohamed101291@gmail.com>
2024-01-27 16:06:35 +0200

    lec-6 init

--- LEC-6/app.js
+++ LEC-6/app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const URI =
     "mongodb+srv://mohamed101291:22546344Mohamed@magcamp.vk31gig.mongodb.net/magCamp?retryWrites=true&w=majority";
-
+const userRouter = require("./routers/users");
 app.use(bodyParser.json());
 
 const connectDB = async () => {
@@ -18,5 +18,7 @@ const connectDB = async () => {
     }
 };
 connectDB();
+
+app.use("/", userRouter);
 
 app.listen(8000);
---
+++ LEC-6/routers/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/api/register");
+router.post("/api/login");
+
+module.exports = router;
